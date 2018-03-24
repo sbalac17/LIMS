80826c9d0ccbbd343cfb3afdb09f7d6952a23587
Shivya Balachandran <sbalac17@my.centennialcollege.ca>
2018-03-24 16:56:36 -0400

    Fix logs

--- LIMS/Controllers/Api/LogsApiController.cs
+++ LIMS/Controllers/Api/LogsApiController.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;
+using System.Web.Http;
 using LIMS.DataAccess;
 using LIMS.Models;
 
@@ -9,12 +12,31 @@ namespace LIMS.Controllers.Api
     [Route("api/logs/")]
     public class LogsApiController : ApiControllerBase
     {
+        // copy of LogEntry with reduced information
+        public class Entry
+        {
+            public string UserId { get; }
+
+            public string Message { get; }
+            
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy h:mm:ss tt}", ApplyFormatInEditMode = true)]
+            public DateTimeOffset Date { get; set; }
+
+            public Entry(LogEntry entry)
+            {
+                UserId = entry.UserId;
+                Message = entry.Message;
+                Date = entry.Date;
+            }
+        }
+
         [Route("api/logs/")]
         [HttpGet]
         [Authorize]
-        public async Task<IEnumerable<LogEntry>> List(int? pageNumber)
+        public async Task<IEnumerable<Entry>> List(int? pageNumber)
         {
-            return await LogsDao.List(this, pageNumber);
+            var results = await LogsDao.List(this, pageNumber);
+            return results.Select(e => new Entry(e));
         }
     }
 }
